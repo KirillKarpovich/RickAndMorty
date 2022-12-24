0e43e25eccf23598b0b0f7e6a87651b915df311c
Kirill Karpovich <kirillkarpovich@Kirills-Air.modem.local>
2022-12-24 18:25:18 +0300

    added search

--- RickAndMorty/VIew/ViewController.swift
+++ RickAndMorty/VIew/ViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UINavigationControllerDelegate {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchResultsUpdating  {
     
     var cards = [Card]()
     let networkManager = NetworkManager()
+    let searchController = UISearchController()
+    var searchResults = [Card]()
     
     
     @IBOutlet var collectionView: UICollectionView!
@@ -19,7 +21,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.viewDidLoad()
         
         collectionView.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-
+        
         networkManager.getData { response in
             DispatchQueue.main.async { [weak self] in
                 self?.cards = response
@@ -27,34 +29,45 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             }
         }
         
+        
         title = "Rick and Morty"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.barTintColor = UIColor.systemGreen
-    
         
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        navigationItem.searchController = searchController
         
         collectionView.dataSource = self
         collectionView.delegate = self
-        navigationController?.delegate = self
-
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        cards.count
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return searchResults.count
+        } else {
+            return cards.count
+        }
     }
+   
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-       guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PictureCell", for: indexPath) as? PictureViewCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PictureCell", for: indexPath) as? PictureViewCell else {
             fatalError("Unable to dequeue PersonCell")
         }
-        let card = cards[indexPath.row]
-        
+        let card: Card
+        if searchController.isActive && searchController.searchBar.text != "" {
+            card = searchResults[indexPath.row]
+        } else {
+            card = cards[indexPath.row]
+        }
         cell.configure(with: card)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController else { return }
-
+        
         let cell = collectionView.cellForItem(at: indexPath) as? PictureViewCell
         
         let image = cell?.imageView.image
@@ -63,9 +76,20 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         vc.image = image
         vc.name = name
         vc.status = status
-
+        
         navigationController?.pushViewController(vc, animated: true)
     }
-
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            searchResults = cards.filter({ (card: Card) -> Bool in
+                return card.name.lowercased().contains(searchText.lowercased())
+            })
+        }
+        collectionView.reloadData()
+    }
+    
 }
+
+
 
